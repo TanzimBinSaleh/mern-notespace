f5cbc04d30d25e2eb635cf884f1ec29d898af4d1
Tanzim Bin Saleh <saleh.tanzim@gmail.com>
2025-07-25 02:45:21 -0500

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 import cors from "cors";
+import path from "path";
 
 // Polyfill fetch for Node.js < 18
 global.fetch = fetch;
@@ -13,16 +14,28 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json()); // middleware to parse json data
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
